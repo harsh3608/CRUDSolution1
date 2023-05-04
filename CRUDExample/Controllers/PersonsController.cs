782a681acd86cbe1d91664ee52c349e1b2e0b131
harsh3608 <harsh3608@gmail.com>
2023-05-04 14:56:04 +0530

    completed till "sort in list view"

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -1,14 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
 
 namespace CRUDExample.Controllers
 {
     public class PersonsController : Controller
     {
+        // Private Fields
+        private readonly IPersonsService _personsService;
+
+        // Constructor
+        public PersonsController(IPersonsService personsService)
+        {
+            _personsService = personsService;
+        }
+
+
         [Route("persons/index")]
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index(string searchBy, string? searchString, 
+            string sortBy = nameof(PersonResponse.PersonName), 
+            SortOrderOptions sortOrder = SortOrderOptions.ASC  )
         {
-            return View();
+            // Searching
+            ViewBag.SearchFields = new Dictionary<string, string>() 
+            {
+                { nameof(PersonResponse.PersonName), "Person Name" },
+                { nameof(PersonResponse.Email), "Email" },
+                { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+                { nameof(PersonResponse.Age), "Age" },
+                { nameof(PersonResponse.Gender), "Gender" },
+                { nameof(PersonResponse.Country), "Country" },
+                { nameof(PersonResponse.Address), "Address" },
+                { nameof(PersonResponse.ReceiveNewsLetters), "Receive News Letters" },
+            } ;
+
+            List<PersonResponse> persons = _personsService.GetFilteredPersons(searchBy, searchString);
+            ViewBag.CurrentSearchBy = searchBy;
+            ViewBag.CurrentSearchString = searchString;
+
+            // Sorting
+            List<PersonResponse> sortedPersons = 
+            _personsService.GetSortedPersons(persons, sortBy, sortOrder);
+            ViewBag.CurrentSortBy = sortBy;
+            ViewBag.CurrentSortOrder = sortOrder.ToString();
+
+            return View(sortedPersons); //Views/Persons/Index.cshtml
         }
     }
 }
